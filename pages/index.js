b6ff03cce096ca71cd243dd64cef2b7b372e510a
migueliti <miguelitirng2004@gmail.com>
2023-12-12 17:08:12 -0500

    pog changes

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>TMG Remodeling</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo_painter.ico" />
       </Head>
 
       <main>
